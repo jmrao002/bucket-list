efbb940a53fbf02040def73de70e5c3b54c983e7
Michael Rao <jmichaelrao@gmail.com>
2021-08-12 20:27:34 -0500

    Updates

---
+++ static/js/main.8e99a319.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpbucket=this.webpackJsonpbucket||[]).push([[0],{9:function(e,t,c){"use strict";c.r(t);var n=c(1),r=c.n(n),o=c(3),s=c.n(o),i=c(0);var d=function(){return Object(i.jsx)("div",{children:"Hello, world!"})};s.a.render(Object(i.jsx)(r.a.StrictMode,{children:Object(i.jsx)(d,{})}),document.getElementById("root"))}},[[9,1,2]]]);
+//# sourceMappingURL=main.8e99a319.chunk.js.map
